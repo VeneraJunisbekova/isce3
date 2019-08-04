de2bce93036cc7e477623d4882c0418387f49d4f
Michael Aïvázis <michael.aivazis@para-sim.com>
2019-08-04 09:05:13 +0300

    isce3.extensions: provided access to the CPU extension module

--- packages/isce3/extensions/__init__.py
+++ packages/isce3/extensions/__init__.py
@@ -1,3 +1,20 @@
 #-*- coding: utf-8 -*-
 
+
+# attempt to
+try:
+    # pull the pure CPU implementation and publish it;
+    from . import isce3 as libisce
+# if this fails
+except ImportError:
+    # there isn't very much we can do; isce3 is not installed correctly
+    import journal
+    # build a description
+    msg = "cannot locate the extension module; is isce3 installed correctly?"
+    # so complain
+    journal.error("isce3").log(msg)
+    # and bail
+    raise SystemExit(1)
+
+
 # end of file
