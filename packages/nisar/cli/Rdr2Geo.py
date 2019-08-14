cdc7e69af1850413e23453e51c6311421bc8fcf2
Michael Aïvázis <michael.aivazis@para-sim.com>
2019-08-14 12:03:29 +0300

    nisar.cli: added {load} and {save} to the {rdr2geo} panel

--- packages/nisar/cli/Rdr2Geo.py
+++ packages/nisar/cli/Rdr2Geo.py
@@ -18,6 +18,38 @@ class Rdr2Geo(nisar.shells.command, family="nisar.cli.rdr2geo"):
     flow.doc = "the workflow to execute"
 
 
+    # load a persisted flow
+    @nisar.export(tip="persist the current flow")
+    def load(self, plexus, **kwds):
+        """
+        Load a flow from a configuration file
+        """
+        # grab a channel
+        channel = plexus.info
+        # show me
+        channel.log(f"loading '{self.flow.pyre_name}'")
+        # show me
+        self.flow.pyre_dump(channel=channel, level=1)
+        # all done
+        return 0
+
+
+    # persist a flow
+    @nisar.export(tip="persist the current flow")
+    def save(self, plexus, **kwds):
+        """
+        Persist the current flow in a configuration file
+        """
+        # grab a channel
+        channel = plexus.info
+        # show me
+        channel.log(f"saving '{self.flow.pyre_name}'")
+        # do it
+        self.flow.save()
+        # all done
+        return 0
+
+
     # configuration information
     @nisar.export(tip="validate and display the workflow configuration")
     def config(self, plexus, **kwds):
