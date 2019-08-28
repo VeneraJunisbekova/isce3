069ad396bbde7d971fbb84bbc4037e02cd3497e6
Michael Aïvázis <michael.aivazis@para-sim.com>
2019-08-28 13:00:10 +0300

    nisar.readers: primed the {slc} reader and its foundry

--- packages/nisar/readers/SLC.py
+++ packages/nisar/readers/SLC.py
@@ -1,15 +1,23 @@
 # -*- coding: utf-8 -*-
 
-# super class
+
+# support
+import nisar
+# superclass
 from .HDF5 import HDF5
 
-# slc hdf5 reader
-class SLC(HDF5, family='nisar.readers.slc'):
-    '''
-    Class for parsing NISAR SLC products into isce structures.
-    '''
 
-    # validation tag to ensure correct product type
-    productValidationType = "SLC"
+# reader for HDF5 files with NISAR SLC
+class SLC(HDF5, family='nisar.readers.slc.slc', implements=nisar.protocols.readers.slc):
+    """
+    Reader of SLCs from NISAR compliant HDF5 files
+    """
 
-# end-of-file
+
+    # output
+    slc = nisar.protocols.products.slc.output()
+    slc.default = nisar.products.slc() # switch the default to the NISAR product
+    slc.doc = "the SLC product extracted by this reader"
+
+
+# end of file
--- packages/nisar/readers/__init__.py
+++ packages/nisar/readers/__init__.py
@@ -1,4 +1,21 @@
 # -*- coding: utf-8 -*-
 
 
+# support
+import nisar
+
+
+# the reader of SLCs
+@nisar.foundry(implements=nisar.protocols.readers.slc,
+               tip="a reader of SLC products in standard NISAR format")
+def slc():
+    """
+    Access the reference implementation of a reader for a NISAR SLC
+    """
+    # pull the factory
+    from .SLC import SLC
+    # and publish it
+    return SLC
+
+
 # end of file
